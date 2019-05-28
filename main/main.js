a08dbcc0f73dde5ef32dd6c61d400db9d0fc73a1
GaoMingye <gaomingye2010@163.com>
2019-05-29 00:59:50 +0800

    ac

--- main/main.js
+++ main/main.js
@@ -1,4 +1,21 @@
 module.exports = function main(inputs) {
     // write your code here...
-    return "price";
+    var distance = inputs.distance
+    var time = inputs.parkTime
+    var price = 0
+    if(distance <=2){
+        price = 6
+    }
+    else if(distance <= 8){
+        price = 6
+        price += (distance-2)*0.8
+    }
+    else{
+        price = 6 + 6*0.8
+        price += (distance-8)*1.2
+    }
+    
+    price += time*0.25
+    
+    return Math.round(price);
 };
